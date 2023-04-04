6dc400f44edd25cf1e2784ffd6e4a820e54678fd
Vanilza <vaflosima@gmail.com>
2023-04-04 20:33:41 -0300

    criação da função createTask

--- DH-FrontEnd2-ToDoApp-ProjetoBase/js/tasks.js
+++ DH-FrontEnd2-ToDoApp-ProjetoBase/js/tasks.js
@@ -1,6 +1,24 @@
 const authToken = localStorage.getItem('userToken');
 const finishSessionRef = document.querySelector('#closeApp');
 const userNameRef = document.querySelector('#userName');
+const taskRef = document.querySelector('#novaTarefa');
+const buttontaskRef = document.querySelector('#criarTarefa');
+
+var userTask = {
+
+    description: '',
+    completed: false
+
+};
+
+function validateTask(task){
+
+    userTask.description = task;
+
+    console.log(userTask)
+}
+
+taskRef.addEventListener('keyup',(event) => validateTask(event.target.value))
 
 
 const requestHeaders = {
@@ -43,5 +61,46 @@ function getUserData() {
 
 }
 
-getUserData();
+
 finishSessionRef.addEventListener('click', () => logout());
+
+
+function verificaToken (){
+
+    if (authToken===null) {
+
+        logout() 
+        alert('Usuário não logado') 
+    }
+
+    else {
+
+        getUserData();
+    }
+}
+
+verificaToken()
+
+function criarTarefa (event){
+
+    event.preventDefault();
+
+    var requestConfig = {
+        method: 'POST',
+        headers: requestHeaders,
+	    body: JSON.stringify(userTask)
+
+    }
+
+    fetch('https://todo-api.ctd.academy/v1/tasks', requestConfig).then(
+        response => {
+            if (response.ok){
+                console.log('tarefa criada')
+            }
+        }
+    )
+
+
+}
+
+buttontaskRef.addEventListener('click',(event) => criarTarefa(event))
