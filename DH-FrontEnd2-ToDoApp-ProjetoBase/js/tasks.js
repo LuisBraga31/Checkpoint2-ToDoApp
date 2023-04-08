b12c9cb9b6c343ba4259646b622fa1ccbc9dac3a
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-04-08 20:27:36 -0300

    Gerenciando as tarefas

--- DH-FrontEnd2-ToDoApp-ProjetoBase/js/tasks.js
+++ DH-FrontEnd2-ToDoApp-ProjetoBase/js/tasks.js
@@ -6,6 +6,9 @@ const userNameRef = document.querySelector('#userName');
 const taskRef = document.querySelector('#novaTarefa');
 const buttontaskRef = document.querySelector('#criarTarefa');
 
+const tasksPendetesRef = document.querySelector('.tarefas-pendentes');
+const tasksFinalizadasRef = document.querySelector('.tarefas-terminadas');
+
 const requestHeaders = {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
@@ -15,9 +18,13 @@ const requestHeaders = {
 var userTask = {
 
     description: '',
-    completed: false
+    completed: false,
+    createdAt: ''
 
 };
+
+let tasksPendentes = [];
+let tasksFinalizadas = [];
 
 /* 02 - Funções */
 
@@ -46,7 +53,6 @@ function validateDescription(task) {
 
 }
 
-
 function getUserData() {
 
     var requestConfig = {
@@ -65,6 +71,7 @@ function getUserData() {
             )
             if(response.ok) {
                 console.log('Ok');
+                receberTarefa();
             }  
         }
     )
@@ -84,13 +91,152 @@ function criarTarefa (event) {
     fetch('https://todo-api.ctd.academy/v1/tasks', requestConfig).then(
         response => {
             if (response.ok){
-                console.log('tarefa criada')
+                console.log('tarefa criada');
+                receberTarefa();
             }
         }
     )
 
 
 }
+
+function receberTarefa() {
+
+    var requestConfig = {
+        method: 'GET',
+        headers: requestHeaders
+    }
+
+    fetch(`https://todo-api.ctd.academy/v1/tasks`, requestConfig).then(
+         response => {
+            if (response.ok) {
+                response.json().then (
+                    tasks => {
+                        insertTasks(tasks);
+                        console.log(tasks);
+                    }
+                )
+            }
+
+         }
+    )
+    
+}
+
+function finalizarTarefa(target) {
+
+    var userUpdate = {
+        completed: true
+    }
+
+    var requestConfig = {
+        method: 'PUT',
+        headers: requestHeaders,
+        body: JSON.stringify(userUpdate)
+    }
+
+    fetch(`https://todo-api.ctd.academy/v1/tasks/${target}`, requestConfig).then (
+        response => {
+            if(response.ok) {
+                receberTarefa();
+                console.log('Atualizou!');
+            }
+        }
+    )
+
+}
+
+function deletarTarefa(target) {
+    
+    var requestConfig = {
+        method: 'DELETE',
+        headers: requestHeaders
+    }
+
+    fetch(`https://todo-api.ctd.academy/v1/tasks/${target}`, requestConfig).then (
+        response => {
+            if(response.ok) {
+                receberTarefa();
+                console.log('Deletou!');
+            }
+        }
+    )
+}
+
+function insertTasks(tasks) {
+
+    tasksPendetesRef.innerHTML = " ";
+    tasksFinalizadasRef.innerHTML = " ";
+    
+    tasksPendentes = [];
+    tasksFinalizadas = [];
+
+    tasks.map( task => {
+
+        if(task.completed) {
+            tasksFinalizadas.push(task);
+        } else {
+            tasksPendentes.push(task);
+        }
+
+    });
+
+    for (let i=0; i < tasksPendentes.length; i++) {
+    
+        tasksPendetesRef.innerHTML += `
+        <li class="tarefa">
+            <div class="not-done"></div>
+            <div class="descricao">
+            <p class="nome"> ${tasksPendentes[i].description}</p>
+            <p class="timestamp">Criada em: 15/07/21</p>
+            </div>
+        </li>
+        `
+    }
+
+    for (let i=0; i < tasksFinalizadas.length; i++) {
+    
+        tasksFinalizadasRef.innerHTML += `
+        <li class="tarefa">
+            <div class="not-done"></div>
+            <div class="descricao">
+            <p class="nome"> ${tasksFinalizadas[i].description}</p>
+            <p class="timestamp">Criada em: 15/07/21</p>
+            </div>
+        </li>
+        `
+    }
+
+    /* ====================================== */
+
+
+    atualizar();
+    deletar();
+
+}
+
+function atualizar() {
+    let completedTaskRef = Array.from(tasksPendetesRef.children);
+
+    completedTaskRef.map( (item, index) => {
+
+        const clickTask = item.children[0];
+        
+        clickTask.addEventListener('click', () => finalizarTarefa(tasksPendentes[index].id));
+    });
+}
+
+function deletar() {
+    let deleteTaskRef = Array.from(tasksFinalizadasRef.children);
+
+    deleteTaskRef.map( (item, index) => {
+
+        const clickTask = item.children[0];
+        
+        clickTask.addEventListener('click', () => deletarTarefa(tasksFinalizadas[index].id));
+    });
+}
+
 
 /* 03 - Eventos */
 
